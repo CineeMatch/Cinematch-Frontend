f981e5731a257f841448386187312c09febaafed
Abdullah Can <abdullahcan123gs@hotmail.com>
2025-05-11 16:00:17 +0300

    WatchedPage Page Modified

--- src/pages/WatchedPage.jsx
+++ src/pages/WatchedPage.jsx
@@ -6,9 +6,6 @@ import MovieGrid from "../components/filmList/MovieGrid.jsx";
 import MovieModal from "../components/home/MovieModal.jsx";
 import { useState } from "react";
 
-
-
-
 export default function WatchedPage(){
 const [openMovieModal, setOpenMovieModal] = useState(false);
   
