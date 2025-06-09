123f52cb27ab8f11cd5c5ab94bfc3fa6ef173007
Abdullah Can <abdullahcan123gs@hotmail.com>
2025-06-09 23:06:09 +0300

    addFriendModal added and connected to endpoint with friend List

---
+++ src/api/profile/friends.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { baseURL } from '../constants';
+
+export const getCurrentUserFriendsList = async () => {
+    try {
+        const token = localStorage.getItem('authToken');
+        const response = await axios.get(`${baseURL}/friend/user`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }).then((response) => {
+            console.log("1111.", response.data);
+            return response;
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching friends list:', error);
+        throw error;
+    }
+}
+
+export const addFriendByNickname = async (nickname) => {
+    try {
+        const token = localStorage.getItem('authToken');
+        const response = await axios.post(`${baseURL}/friend/create`, { nickname }, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error adding friend:', error);
+        throw error;
+    }
+}
---
+++ src/api/profile/user.js
@@ -0,0 +1,17 @@
+// import axios from 'axios';
+// import { baseURL } from '../constants';
+
+// export const getActiveUser = async (userId) => {
+//     try {
+//         const token = localStorage.getItem('authToken');
+//         const response = await axios.get(`${baseURL}/user`, {
+//             headers: {
+//                 Authorization: `Bearer ${token}`,
+//             },
+//         });
+//         return response.data;
+//     } catch (error) {
+//         console.error('Error fetching user profile:', error);
+//         throw error;
+//     }
+// }
