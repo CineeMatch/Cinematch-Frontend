56cf6acc8c004df5bf012e7f65ee858674c434f5
Abdullah Can <abdullahcan123gs@hotmail.com>
2025-06-12 22:03:26 +0300

    Searchbar Filtering Added

--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -1,33 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import { Box, Typography, Button, TextField, Avatar, Grid, Card, CardContent, Stack, InputAdornment, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { useEffect, useState } from 'react'
+import {
+  Box, Typography, Button, TextField, Avatar, Grid, Card, CardContent, Stack,
+  InputAdornment, FormControl, InputLabel, Select, MenuItem, IconButton
+} from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import { getCurrentUserFriendsList } from '../api/profile/friends';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import { deleteFriendById, getCurrentUserFriendsList } from '../api/profile/friends';
 import AddFriendModal from '../modals/profile/AddFriendModal';
 
 const Friends = () => {
 
   const [type, setType] = useState(0);
   const [friends, setFriends] = useState([]);
-  const [IsShowAddFriendModal, setIsShowAddFriendModal] = useState(false); 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [IsShowAddFriendModal, setIsShowAddFriendModal] = useState(false);
 
-  useEffect(() => {
-
-    const fetchFriends = async () => {
-      try {
-        const response = await getCurrentUserFriendsList();
-        setFriends(response);
-      } catch (error) {
-        console.error('Error fetching friends:', error);
-      }
-    };
-    fetchFriends();
-  }
-  , []);
+  const fetchFriends = async () => {
+    try {
+      const response = await getCurrentUserFriendsList();
+      setFriends(response);
+    } catch (error) {
+      console.error('Error fetching friends:', error);
+    }
+  };
 
   const handleTypeChange = (event) => {
-      setType(event.target.value);
-  }
+    setType(event.target.value);
+  };
+
+  const handleDeleteFriend = (id) => {
+    const deleteFriend = async (id) => {
+      try {
+        await deleteFriendById(id);
+        console.log(`Friend with ID ${id} deleted`);
+        fetchFriends(); // Silinince güncelle
+      } catch (error) {
+        console.error('Error deleting friend:', error);
+      }
+    };
+    deleteFriend(id);
+  };
+
+  useEffect(() => {
+    fetchFriends();
+  }, []);
+
+  // 🔍 Search filtreleme işlemi
+  const filteredFriends = friends.filter((friend) =>
+    friend.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+  );
 
   return (
     <Box
@@ -70,6 +92,8 @@ const Friends = () => {
           fullWidth
           variant="outlined"
           placeholder="Search friends by name or game"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -80,129 +104,91 @@ const Friends = () => {
           sx={{
             mb: 3,
             '& .MuiOutlinedInput-root': {
-            '& fieldset': {
-                borderColor: 'rgba(255, 0, 0, 0.5)', // normal border rengi
+              '& fieldset': { borderColor: 'rgba(255, 0, 0, 0.5)' },
+              '&:hover fieldset': { borderColor: 'red' },
+              '&.Mui-focused fieldset': { borderColor: '#f44336' },
             },
-            '&:hover fieldset': {
-                borderColor: 'red', // hover durumunda border
-            },
-            '&.Mui-focused fieldset': {
-                borderColor: '#f44336', // focus (yazarken) border rengi
-            },
-            },
-            '& .MuiInputLabel-root.Mui-focused': {
-            color: '#f44336' // label rengi focus olduğunda
-            },
-            input: { color: 'white' }, fieldset: { borderColor: 'gray' } 
-            
-        }}
+            '& .MuiInputLabel-root.Mui-focused': { color: '#f44336' },
+            input: { color: 'white' }, fieldset: { borderColor: 'gray' }
+          }}
         />
 
         <Box display="flex" justifyContent="space-between" alignItems="center" mb={2}>
-            <Typography variant="h6" gutterBottom>
-            {/* Offline */}
-                Friends
-            </Typography>
-            <FormControl
-                sx={{
-                    m: 1,
-                    minWidth: 120,
-                    '& .MuiOutlinedInput-root': {
+          <Typography variant="h6" gutterBottom>Friends</Typography>
+          <FormControl
+            sx={{
+              m: 1,
+              minWidth: 120,
+              '& .MuiOutlinedInput-root': {
+                color: 'white',
+                '& fieldset': { borderColor: 'white' },
+                '&:hover fieldset': { borderColor: 'white' },
+                '&.Mui-focused fieldset': { borderColor: 'white' },
+              },
+              '& .MuiInputLabel-root': { color: 'white' },
+              '& .MuiInputLabel-root.Mui-focused': { color: 'white' },
+              '& .MuiSelect-icon': { color: 'white' },
+            }}
+            size="small"
+          >
+            <InputLabel id="demo-select-small-label">Type</InputLabel>
+            <Select
+              labelId="demo-select-small-label"
+              id="demo-select-small"
+              value={type}
+              label="Mixed"
+              onChange={handleTypeChange}
+              MenuProps={{
+                PaperProps: {
+                  sx: {
+                    backgroundColor: 'transparent',
+                    backdropFilter: 'blur(5px)',
                     color: 'white',
-                    '& fieldset': {
-                        borderColor: 'white',
-                    },
-                    '&:hover fieldset': {
-                        borderColor: 'white',
-                    },
-                    '&.Mui-focused fieldset': {
-                        borderColor: 'white',
-                    },
-                    },
-                    '& .MuiInputLabel-root': {
-                    color: 'white',
-                    },
-                    '& .MuiInputLabel-root.Mui-focused': {
-                    color: 'white',
-                    },
-                    '& .MuiSelect-icon': {
-                    color: 'white',
-                    },
-                }}
-                size="small"
-                >
-                <InputLabel id="demo-select-small-label">Type</InputLabel>
-                <Select
-                    labelId="demo-select-small-label"
-                    id="demo-select-small"
-                    value={type}
-                    label="Mixed"
-                    onChange={handleTypeChange}
-                    MenuProps={{
-                    PaperProps: {
-                        sx: {
-                        backgroundColor: 'transparent', // transparan arka plan
-                        backdropFilter: 'blur(5px)', // opsiyonel: hafif blur efekti
-                        color: 'white', // yazı rengi beyaz
-                        boxShadow: 'none', // gölgeyi kaldır
-                        },
-                    },
-                    }}
-                >
-                    <MenuItem value={0}>Mixed</MenuItem>
-                    <MenuItem value={1}>Serial</MenuItem>
-                </Select>
-            </FormControl>
+                    boxShadow: 'none',
+                  },
+                },
+              }}
+            >
+              <MenuItem value={0}>Mixed</MenuItem>
+              <MenuItem value={1}>Serial</MenuItem>
+            </Select>
+          </FormControl>
         </Box>
-        
-        {!type ? (
+
         <Grid container spacing={2}>
-          {friends.map((friend,index) => (
-            <Grid item xs={6} md={4} key={index}>
-              <Card sx={{ backgroundColor: 'rgba(255,255,255,0.05)', color: 'white' }}>
-                <CardContent>
-                  <Stack direction="row" spacing={2} alignItems="center">
-                    <Avatar src={friend.avatar} alt={friend.name} />
-                     <Box textAlign="left">
-                      <Typography sx={{ fontSize: '1rem', textAlign: 'left', fontWeight: "bold" }}>{friend.name}</Typography>
-                      <Typography variant="body2" sx={{ color: 'gray', textAlign: 'left' }}>
-                        Last Online {friend.lastOnline}
-                      </Typography>
+          {filteredFriends.map((friend, index) => (
+            <Grid item xs={type ? 12 : 6} md={type ? 12 : 4} key={index}>
+              <Card sx={{ backgroundColor: 'rgba(255,255,255,0.05)', color: 'white', px: type ? 1 : 0 }}>
+                <CardContent sx={{ p: type ? 1 : 2, mt: 1 }}>
+                  <Box display="flex" justifyContent="space-between" alignItems="center">
+                    <Box display="flex" alignItems="center">
+                      <Avatar src={friend.avatar} alt={friend.name} sx={{ width: 48, height: 48, mr: 2 }} />
+                      <Box textAlign="left">
+                        <Typography sx={{ fontSize: '1rem', fontWeight: 'bold' }}>{friend.name}</Typography>
+                        <Typography variant="body2" sx={{ color: 'gray' }}>
+                          Last Online {friend.lastOnline}
+                        </Typography>
+                      </Box>
                     </Box>
-                  </Stack>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-        ):(
-        <Grid container spacing={2}>
-          {friends.map((friend, index) => (
-            <Grid item xs={12} key={index}>
-              <Card sx={{ backgroundColor: 'rgba(255,255,255,0.05)', color: 'white', px: 1 }}>
-                <CardContent sx={{ p: 1 }}>
-                  <Box display="flex" alignItems="center">
-                    <Avatar src={friend.avatar} alt={friend.name} sx={{ width: 48, height: 48, mr: 2 }} />
-                    <Box textAlign="left">
-                      <Typography sx={{ fontSize: '1rem', textAlign: "left", fontWeight: "bold" }}>{friend.name}</Typography>
-                      <Typography variant="body2" sx={{ color: 'gray', textAlign: 'left' }}>
-                        Last Online {friend.lastOnline}
-                      </Typography>
-                    </Box>
+                    <IconButton
+                      onClick={() => handleDeleteFriend(friend.id)}
+                      sx={{ color: '#f44336', '&:hover': { color: '#d32f2f' } }}
+                    >
+                      <RemoveCircleOutlineIcon />
+                    </IconButton>
                   </Box>
                 </CardContent>
               </Card>
             </Grid>
           ))}
         </Grid>
-        )}
-        
+
       </Box>
-      { IsShowAddFriendModal && (
-        <AddFriendModal setIsShowAddFriendModal={setIsShowAddFriendModal}/>
+      {IsShowAddFriendModal && (
+        <AddFriendModal setIsShowAddFriendModal={setIsShowAddFriendModal} />
       )}
     </Box>
-  )
-}
+  );
+};
 
-export default Friends
+export default Friends;
